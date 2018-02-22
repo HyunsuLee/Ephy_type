e36b74747157769f60e65a5292feb0ba84d49c0d
Hyunsu Lee <hyunspookey@gmail.com>
2018-02-22 21:17:48 +0900

    leakrelu appiled.

--- Clustering/ANNtest_allen_tensorboard.py
+++ Clustering/ANNtest_allen_tensorboard.py
@@ -25,7 +25,7 @@ with tf.name_scope('layer1'):
     W1 = tf.Variable(tf.truncated_normal([43, 20], stddev=Xavier_init), name='W1')
     # ReLU function for layer 1. 20 nodes.
     b1 = tf.Variable(tf.random_normal([20]))
-    L1 = tf.nn.relu(tf.matmul(X, W1) + b1)
+    L1 = tf.nn.leaky_relu(tf.matmul(X, W1) + b1)
     L1 = tf.nn.dropout(L1, keep_prob)
     tf.summary.histogram("W1", W1)
 
@@ -33,7 +33,7 @@ with tf.name_scope('layer2'):
     Xavier_init = np.sqrt(2) * np.sqrt(2 / (20 + 10))
     W2 = tf.Variable(tf.truncated_normal([20, 10], stddev=Xavier_init), name='W2')
     b2 = tf.Variable(tf.random_normal([10]))
-    L2 = tf.nn.relu(tf.matmul(L1, W2) + b2)
+    L2 = tf.nn.leak_relu(tf.matmul(L1, W2) + b2)
     L2 = tf.nn.dropout(L2, keep_prob)
     tf.summary.histogram("W2", W2)
 
@@ -66,7 +66,7 @@ saver = tf.train.Saver()
 sess = tf.Session()
 
 sess.run(tf.global_variables_initializer())
-log_path = '/180222/LR_e-4_100000_dropout/'
+log_path = '/180222/leakyrelu_25000_dropout/'
 summaries_dir = './logs/' + log_path
 train_writer = tf.summary.FileWriter(summaries_dir + '/train')
 test_writer = tf.summary.FileWriter(summaries_dir + '/test')
@@ -76,7 +76,7 @@ train_writer.add_graph(sess.graph)
 # batch_size = 128
 # total_batch = int(len(trainX) / batch_size)
 
-for epoch in range(100000):
+for epoch in range(25000):
     # for start, end in zip(range(0, len(trainX), batch_size),
     #    range(batch_size, len(trainX)+1, batch_size)):
     #    sess.run(optimizer, feed_dict={X: trainX[start:end], Y: trainY[start:end]})
