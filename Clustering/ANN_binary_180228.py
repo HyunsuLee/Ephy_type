0a929fb5441d2221396511cf9d2bcfcfb0be3a79
Hyunsu Lee <hyunspookey@gmail.com>
2018-03-01 08:42:18 +0900

    learning rate assign in tf.placeholder
    testing assign float varialble in feed dict

--- Clustering/ANN_binary_180228.py
+++ Clustering/ANN_binary_180228.py
@@ -26,7 +26,7 @@ created by data_processing_180227.ipynb
     _ramp.csv - ramp pulse protocol
 3 X 4 = 12. 12 different ANN models will be created.
 """
-log_path = '/180228binary_full/bn_wrapper_test/'
+log_path = '/180228binary_full/LR_placeholder_test/'
 summaries_dir = './logs/' + log_path # for tensorboard summary
 model_dir = './model/' + log_path # for model saver
 
@@ -41,6 +41,7 @@ X = tf.placeholder(tf.float32, [None, 43]) # for full model, 43 input features
 Y = tf.placeholder(tf.float32, [None, 2]) # binary E vs I class
 keep_prob = tf.placeholder(tf.float32)
 is_training = tf.placeholder(tf.int32)
+learning_rate = tf.placehoder(tf.float32)
 epsilon = 1e-3 # for Batch normalization
 layer1_shape = [43, 20]
 layer2_shape = [20, 10]
@@ -104,7 +105,7 @@ with tf.name_scope('optimizer'):
     # lossL2 =  tf.reduce_mean(tf.nn.l2_loss(W1) + 
     # tf.nn.l2_loss(W2) + tf.nn.l2_loss(W3))* 0.01
     cost = base_cost # + lossL2 
-    optimizer = tf.train.AdamOptimizer(0.001).minimize(cost)
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
     tf.summary.scalar('cost', cost)
 
 with tf.name_scope("accuracy"):
@@ -127,25 +128,30 @@ train_writer.add_graph(sess.graph)
 # batch_size = 128
 # total_batch = int(len(trainX) / batch_size)
 
+random_learning_rate = 0.001
 for epoch in range(5000):
     # for start, end in zip(range(0, len(trainX), batch_size),
     #    range(batch_size, len(trainX)+1, batch_size)):
     #    sess.run(optimizer, feed_dict={X: trainX[start:end], Y: trainY[start:end]})
-    sess.run(optimizer, feed_dict={X: trainX, Y: trainY, keep_prob: 0.5, is_training: 1})
+    sess.run(optimizer, feed_dict={X: trainX, Y: trainY, keep_prob: 0.5, 
+                        is_training: 1, learning_rate: random_learning_rate})
     if (epoch % 5) == 0:
         train_acc, train_summ = sess.run([accuracy, summ], 
-                        feed_dict={X: trainX, Y: trainY, keep_prob: 1.0, is_training: 1})
+                        feed_dict={X: trainX, Y: trainY, keep_prob: 1.0, 
+                        is_training: 1, learning_rate: random_learning_rate})
         train_writer.add_summary(train_summ, epoch)
 
         test_acc, test_summ = sess.run([accuracy, summ], 
-                        feed_dict={X: testX, Y: testY, keep_prob: 1.0, is_training: 0})
+                        feed_dict={X: testX, Y: testY, keep_prob: 1.0, 
+                        is_training: 0, learning_rate: random_learning_rate})
         test_writer.add_summary(test_summ, epoch)
 
     if (epoch % 500) == 0:
         saver.save(sess, model_dir + '/ANN.ckpt', epoch)
         print('Epoch:', '%04d' % (epoch +1))
         test_cost = sess.run(cost, 
-                        feed_dict={X: testX, Y: testY, keep_prob: 1.0, is_training: 0})
+                        feed_dict={X: testX, Y: testY, keep_prob: 1.0, 
+                        is_training: 0, learning_rate: random_learning_rate})
         print('Test cost:', '{:.3f}'.format(test_cost))
         print('Test accuracy:', '{:.3f}'.format(test_acc))
 
