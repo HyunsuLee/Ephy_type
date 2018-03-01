40de492a54c15922098289e3cdc6a8ba702cbb06
Hyunsu Lee <hyunspookey@gmail.com>
2018-03-01 10:16:04 +0900

    tf.contrib.layer batch norm test.

--- Clustering/ANN_binary_180228.py
+++ Clustering/ANN_binary_180228.py
@@ -26,7 +26,7 @@ created by data_processing_180227.ipynb
     _ramp.csv - ramp pulse protocol
 3 X 4 = 12. 12 different ANN models will be created.
 """
-log_path = '/180228binary_full/BN_control_denpendency_test/'
+log_path = '/180228binary_full/BN_tflayer_test/'
 summaries_dir = './logs/' + log_path # for tensorboard summary
 model_dir = './model/' + log_path # for model saver
 
@@ -40,7 +40,7 @@ testY = np.loadtxt(data_path + 'BtestY.csv', delimiter = ',')
 X = tf.placeholder(tf.float32, [None, 43]) # for full model, 43 input features
 Y = tf.placeholder(tf.float32, [None, 2]) # binary E vs I class
 keep_prob = tf.placeholder(tf.float32)
-is_training = tf.placeholder(tf.int32)
+is_training_holder = tf.placeholder(tf.bool)
 learning_rate = tf.placeholder(tf.float32)
 epsilon = 1e-3 # for Batch normalization
 layer1_shape = [43, 20]
@@ -53,6 +53,7 @@ def weight_init(shape, name_for_weight):
     weights = tf.truncated_normal(shape, stddev = Xavier_init)
     return tf.Variable(weights, name = name_for_weight)
 
+'''
 def batch_norm_wrapper(inputs, is_training, decay = 0.999):
 
     scale = tf.Variable(tf.ones([inputs.get_shape()[-1]]))
@@ -72,12 +73,14 @@ def batch_norm_wrapper(inputs, is_training, decay = 0.999):
     else:
         return tf.nn.batch_normalization(inputs,
             pop_mean, pop_var, beta, scale, epsilon)
-
+'''
 
 with tf.name_scope('layer1'):
     W1 = weight_init(layer1_shape, 'W1')
     z1 = tf.matmul(X, W1)
-    BN1 = batch_norm_wrapper(z1, is_training) 
+    # BN1 = batch_norm_wrapper(z1, is_training)
+    BN1 = tf.contrib.layers.batch_norm(z1, center = True, scale = True
+          is_training = is_training_holder) 
     L1 = tf.nn.relu(BN1)
     L1 = tf.nn.dropout(L1, keep_prob)
     tf.summary.histogram('W1', W1)
@@ -85,7 +88,9 @@ with tf.name_scope('layer1'):
 with tf.name_scope('layer2'):
     W2 = weight_init(layer2_shape, 'W2')
     z2 = tf.matmul(L1, W2)
-    BN2 = batch_norm_wrapper(z2, is_training)
+    # BN2 = batch_norm_wrapper(z2, is_training)
+    BN2 = tf.contrib.layers.batch_norm(z2, center = True, scale = True
+          is_training = is_training_holder) 
     L2 = tf.nn.relu(BN2)
     L2 = tf.nn.dropout(L2, keep_prob)
     tf.summary.histogram('W2', W2)
@@ -137,16 +142,16 @@ for epoch in range(5000):
     #    range(batch_size, len(trainX)+1, batch_size)):
     #    sess.run(optimizer, feed_dict={X: trainX[start:end], Y: trainY[start:end]})
     sess.run(optimizer, feed_dict={X: trainX, Y: trainY, keep_prob: 0.5, 
-                        is_training: 1, learning_rate: random_learning_rate})
+                        is_training_holder: 1, learning_rate: random_learning_rate})
     if (epoch % 5) == 0:
         train_acc, train_summ = sess.run([accuracy, summ], 
                         feed_dict={X: trainX, Y: trainY, keep_prob: 1.0, 
-                        is_training: 1, learning_rate: random_learning_rate})
+                        is_training_holder: 1, learning_rate: random_learning_rate})
         train_writer.add_summary(train_summ, epoch)
 
         test_acc, test_summ = sess.run([accuracy, summ], 
                         feed_dict={X: testX, Y: testY, keep_prob: 1.0, 
-                        is_training: 0, learning_rate: random_learning_rate})
+                        is_training_holder: 0, learning_rate: random_learning_rate})
         test_writer.add_summary(test_summ, epoch)
 
     if (epoch % 500) == 0:
@@ -154,7 +159,7 @@ for epoch in range(5000):
         print('Epoch:', '%04d' % (epoch +1))
         test_cost = sess.run(cost, 
                         feed_dict={X: testX, Y: testY, keep_prob: 1.0, 
-                        is_training: 0, learning_rate: random_learning_rate})
+                        is_training_holder: 0, learning_rate: random_learning_rate})
         print('Test cost:', '{:.3f}'.format(test_cost))
         print('Test accuracy:', '{:.3f}'.format(test_acc))
 
