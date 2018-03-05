5ca35c6df7fd2e99aa5f591112961405b1152869
HyunsuLee <hyunspookey@gmail.com>
2018-03-05 18:56:47 +0900

    excitatory line models coarse test
    1. full input model completed

--- Clustering/coarse_searching.bash
+++ Clustering/coarse_searching.bash
@@ -1,2 +1,4 @@
-python 02_1_binary_short_coarse_searching.py &&
-python 03_1_binary_ramp_coarse_searching.py
+python3 04_1_Eline_full_coarse_searching.py &&
+python3 05_1_Eline_long_coarse_searching.py &&
+python3 06_1_Eline_short_coarse_searching.py &&
+python3 07_1_Eline_ramp_coarse_searching.py
