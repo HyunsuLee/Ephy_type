25de279fa45266474a00ba3d3f809570452997db
HyunsuLee <hyunspookey@gmail.com>
2018-03-06 15:41:59 +0900

    inhibitory line hyperparameter coarse searching code

--- Clustering/coarse_searching.bash
+++ Clustering/coarse_searching.bash
@@ -1,4 +1,4 @@
-python3 04_1_Eline_full_coarse_searching.py &&
-python3 05_1_Eline_long_coarse_searching.py &&
-python3 06_1_Eline_short_coarse_searching.py &&
-python3 07_1_Eline_ramp_coarse_searching.py
+python3 08_1_Iline_full_coarse_searching.py &&
+python3 09_1_Iline_long_coarse_searching.py &&
+python3 10_1_Iline_short_coarse_searching.py &&
+python3 11_1_Iline_ramp_coarse_searching.py
